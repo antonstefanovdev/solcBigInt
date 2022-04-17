dc9876843caefb1de438fe724157e7a040b82d7c
antonstefanovdev <antonstefanovdev@gmail.com>
2022-04-18 00:01:43 +0300

    basic methods to work with BigInt and SignState added

--- contracts/solcbigint.sol
+++ contracts/solcbigint.sol
@@ -18,8 +18,7 @@ library SolcBigInt {
     */
     /// @notice Saves integers as uint arrays with some additional data (such as sign and isZero check)
     /// @dev has data field which is uint array and signState field which is enum SignState
-    struct BigInt
-    {
+    struct BigInt {
         uint[] data;
         SignState signState;
     }
@@ -33,21 +32,41 @@ library SolcBigInt {
     * Should be an integer in (-2**255, 2**255-1)
     */
     /// @return bigInt Express value as a BigInt structure
-
-        function initBI(int value) public returns (BigInt memory bigInt)
-    {
+    function initBI(int value) public returns (BigInt memory bigInt) {
         uint[] memory data = new uint[](1);
-
+        if(value >= 0)
+            data[0] = uint(value);
+        else
+            data[0] = uint(-value);
+            
         BigInt memory result;
         result.data = data;
         result.signState = getSignStateByVal(value);
 
-        return result;
+        bigInt = result;
+    }
+
+    /// @notice A function that converts basic uint256 value into BigInt
+    /** @dev All of arithmetical functions here works only with BigInt structure
+    * To do safe arithmetical operations with uint256 and correctly save result when it is greater 
+    * than 2**256-1 or less then 0 it is recomended to init BigInt for each argument first
+    */
+    /** @param value Express a value which we want to store in new BigInt variable
+    * Should be an integer in (0, 2**256-1)
+    */
+    /// @return bigInt Express value as a BigInt structure
+    function initBI(uint value) public returns (BigInt memory bigInt) {
+        uint[] memory data = new uint[](1);
+
+        BigInt memory result;
+        result.data = data;
+        result.signState = SignState.Positive;
+
+        bigInt = result;
     }
 
     /// @dev A private function to get correst SignState item by int256 value
-    function getSignStateByVal(int value) private returns (SignState)
-    {
+    function getSignStateByVal(int value) private returns (SignState) {
         if(value == 0)
         return SignState.Zero;
         else if (value > 0)
@@ -57,8 +76,90 @@ library SolcBigInt {
     }
 
     //Todo: add NatSpec
-    function getSingStateBI(BigInt memory bigInt) public returns(SignState signState)
-    {
+    function getSingStateBI(BigInt memory bigInt) public returns(SignState signState) {
         return bigInt.signState;
+    }
+
+    function isPositive(BigInt memory bigInt) public returns(bool flag) {
+        return bigInt.signState == SignState.Positive;
+    }
+
+    function isNegative(BigInt memory bigInt) public returns(bool flag) {
+        return bigInt.signState == SignState.Negative;
+    }
+
+    function isPositiveOrZero(BigInt memory bigInt) public returns(bool flag) {
+        return !isNegative(bigInt);
+    }
+
+    function isNegativeOrZero(BigInt memory bigInt) public returns(bool flag) {
+        return !isPositive(bigInt);
+    }
+
+    function isZero(BigInt memory bigInt) public returns(bool flag) {
+        return bigInt.signState == SignState.Zero;
+    }
+
+    function isApplicableToInt256(BigInt memory bigInt) public returns(bool flag) {
+        if(isZero(bigInt))
+        flag = true;
+        else if(bigInt.data.length == 1) {
+            if(bigInt.signState == SignState.Negative && bigInt.data[0] > uint256(-type(int256).min))
+            flag = false;
+            else if(bigInt.signState == SignState.Positive && bigInt.data[0] > uint256(type(int256).max))  
+            flag = false;
+            else
+            flag = true;          
+        }
+        else
+        flag = false;
+    }
+
+    function isApplicableToUInt256(BigInt memory bigInt) public returns(bool flag) {
+        if(isNegative(bigInt))
+        flag = false;
+        else if(isZero(bigInt))
+        flag = true;
+        else if(bigInt.data.length == 1)
+        flag = true;
+        else
+        flag = false;
+    }
+
+        function getSignMultiplier(BigInt memory bigInt) private returns(int) {
+        if(isPositive(bigInt))
+        return 1;
+        else if(isNegative(bigInt))
+        return -1;
+        else
+        return 0;
+    }
+
+    function convertToInt256FromBI(BigInt memory bigInt) public returns(int256 int256Value) {
+        if(isZero(bigInt))
+        int256Value = 0;
+        else
+        int256Value = int256(bigInt.data[0]) * getSignMultiplier(bigInt);
+    }
+
+    function convertToUInt256FromBI(BigInt memory bigInt) public returns(uint256 uInt256Value) {
+        if(isZero(bigInt))
+        uInt256Value = 0;
+        else
+        uInt256Value = bigInt.data[0];
+    }
+
+    function safeConvertToInt256FromBI(BigInt memory bigInt) public returns(int256 int256Value) {
+        if(isApplicableToInt256(bigInt))
+        int256Value = convertToInt256FromBI(bigInt);
+        else
+        int256Value = 0;
+    }
+
+    function safeConvertToUInt256FromBI(BigInt memory bigInt) public returns(uint256 uInt256Value) {
+        if(isApplicableToUInt256(bigInt))
+        uInt256Value = convertToUInt256FromBI(bigInt);
+        else
+        uInt256Value = 0;
     }
 }
